ed62b1a71c75921f6c2557c31a809cc2deb6640f
Judith L. <judith.lenz@outlook.de>
2025-02-18 09:40:40 +0100

    show all players from array

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.scss']
+  styleUrls: ['./player.component.scss'],
 })
 export class PlayerComponent {
-
+  @Input() name = '';
+  //das geben wir in die component mit rein.
+  // Also dann als Variable im html verwendbar.
 }
